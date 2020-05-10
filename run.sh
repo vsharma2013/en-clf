50ab5069504f5b8a05a94ebe52c348695cefb90a
Vishal Sharma <vsharma@ogeestudio.com>
2020-05-10 19:58:23 +0530

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pip install gensim
 pip install s3fs
-/usr/bin/python3 train_multi_label.py 
+/usr/bin/python3 train_multi_label.py --batch_size 128 --epochs 1 --learning_rate 0.01 --model_dir /opt/ml/model
